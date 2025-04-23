2484e9a5b05f67100ed1537147f7d37735721531
yubing <2175183649@qq.com>
2025-04-23 21:46:21 +0800

    Java简单项目

---
+++ java-learn-hub-master/javapass/extendsdemo/src/template_type/Test.java
@@ -0,0 +1,10 @@
+package template_type;
+
+public class Test {
+    public static void main(String[] args) {
+        People A=new Teacher();
+        People B=new Student();
+        A.write();
+        B.write();
+    }
+}
