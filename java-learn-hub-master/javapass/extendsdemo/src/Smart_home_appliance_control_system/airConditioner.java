818c0e39541a3eac186dd55c51e1542f5f5ae576
yubing <2175183649@qq.com>
2025-04-25 21:32:49 +0800

    Delete java-learn-hub-master/javapass/extendsdemo directory

--- java-learn-hub-master/javapass/extendsdemo/src/Smart_home_appliance_control_system/airConditioner.java
+++
@@ -1,8 +0,0 @@
-package Smart_home_appliance_control_system;
-
-public class airConditioner extends home_app {
-    public airConditioner() {}
-    public airConditioner(String name,boolean status) {
-        super(name, status);
-    }
-}
