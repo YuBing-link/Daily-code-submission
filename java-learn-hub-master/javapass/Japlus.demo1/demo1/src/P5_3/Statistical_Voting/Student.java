64b53517b8037427008ec34e7c8bc3e9e38b4bd6
yubing <2175183649@qq.com>
2025-05-03 20:50:06 +0800

    Add files via upload

---
+++ java-learn-hub-master/javapass/Japlus.demo1/demo1/src/P5_3/Statistical_Voting/Student.java
@@ -0,0 +1,13 @@
+package P5_3.Statistical_Voting;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private  String wantname;
+}
