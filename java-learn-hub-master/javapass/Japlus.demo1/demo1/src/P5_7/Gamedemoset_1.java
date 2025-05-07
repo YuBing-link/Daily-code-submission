46ae06206f80dc08cd762d5b7a7e670a2ae89ba8
yubing <2175183649@qq.com>
2025-05-07 18:49:15 +0800

    Test of dealing cards based on the principle of shuffling cards

---
+++ java-learn-hub-master/javapass/Japlus.demo1/demo1/src/P5_7/Gamedemoset_1.java
@@ -0,0 +1,14 @@
+package P5_7;
+
+public class Gamedemoset_1 {
+    //Card,Room,
+
+    public static void main(String[] args) {
+        Room room = new Room();
+        room.start();
+    }
+
+
+
+
+}
