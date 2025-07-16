fd7b81564de7839948bec4fd727de21d98945c06
YuBing-link <2175183649@qq.com>
2025-07-16 19:15:45 +0800

    new

---
+++ java-learn-hub-master/javapass/springboot.demo1/EmoDemo/src/main/java/com/nvoe/emodemo/EmoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nvoe.emodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoDemoApplication.class, args);
+    }
+
+}
