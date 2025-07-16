edadec2ecf5253f1d5a96015212f1df1b15f5504
YuBing-link <2175183649@qq.com>
2025-07-16 19:15:45 +0800

    new

---
+++ java-learn-hub-master/javapass/springboot.demo1/EmoDemo/src/test/java/com/nvoe/emodemo/EmoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvoe.emodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
