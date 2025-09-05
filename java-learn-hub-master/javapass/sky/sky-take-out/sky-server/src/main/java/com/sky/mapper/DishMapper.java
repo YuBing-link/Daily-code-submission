b458ed95c27b9893af976743ed7397c06c28ab79
YuBing-link <2175183649@qq.com>
2025-09-05 20:55:06 +0800

    菜品模块

--- java-learn-hub-master/javapass/sky/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ java-learn-hub-master/javapass/sky/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -18,4 +24,10 @@ public interface DishMapper {
     Integer countByCategoryId(Long categoryId);
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    @Select("select status from dish where id=#{id}")
+    Integer getById(Long id);
+    @Delete("delete from dish where id=#{id}")
+    void delete(Long id);
 }
--- java-learn-hub-master/javapass/sky/sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ java-learn-hub-master/javapass/sky/sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorsMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetMealDishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +29,8 @@ public class DishServiceImpl implements DishService {
     DishMapper dishMapper;
     @Autowired
     DishFlavorsMapper dishFlavorsMapper;
+    @Autowired
+    SetMealDishMapper setMealDishMapper;
     @Override
     @Transactional
     public void dishWithFlavors(DishDTO dishDTO) {
@@ -35,6 +46,37 @@ public class DishServiceImpl implements DishService {
                 dishFlavor.setDishId(dishID);
             });
             dishFlavorsMapper.insertBatch(flavors);
+        }
+
+
+    }
+
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(),dishPageQueryDTO.getPageSize());
+        Page<DishVO> page=dishMapper.pageQuery(dishPageQueryDTO);
+
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    @Override
+    @Transactional
+    public void deleteBatch(List<Long> ids) {
+        //判断菜品是否可以删除--条件：起售，关联套餐
+        for(Long id:ids){
+            int status = dishMapper.getById(id);
+            if(status == StatusConstant.ENABLE){
+               throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+        List<Long> setMealDishIdsByDishId = setMealDishMapper.getSetMealDishIdsByDishId(ids);
+        if (setMealDishIdsByDishId!=null&&!setMealDishIdsByDishId.isEmpty()){
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+        //删除菜品操作以及口味
+        for (Long id:ids){
+            dishMapper.delete(id);
+            dishFlavorsMapper.delete(id);
         }
 
 
